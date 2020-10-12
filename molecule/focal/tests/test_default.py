d2a6350dbc94f8c5c5d535157cf42a0dcc6840e8
Daniel <daniel.hoherd@gmail.com>
2020-10-11 22:10:13 -0700

    Allow install to any version even when already installed. (#5)

--- molecule/focal/tests/test_default.py
+++ molecule/focal/tests/test_default.py
@@ -1,8 +1,18 @@
 import os
+from pytest import fixture
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts("all")
+
+
+@fixture
+def get_vars(host):
+    defaults_files = "file=../../defaults/main.yml name=role_defaults"
+    vars_files = "file=../../vars/main.yml name=role_vars"
+    ansible_vars = host.ansible("include_vars", defaults_files)["ansible_facts"]["role_defaults"]
+    ansible_vars.update(host.ansible("include_vars", vars_files)["ansible_facts"]["role_vars"])
+    return ansible_vars
 
 
 def test_exiftool(host):
@@ -12,3 +22,8 @@ def test_exiftool(host):
     assert exiftool_command.user == "root"
     assert exiftool_command.group == "root"
     assert exiftool_command.mode == 0o555
+
+
+def test_correct_exiftool_version(get_vars, host):
+    ver = host.run("exiftool -Ver")
+    assert ver.stdout.strip() == get_vars["exiftool_version"]
